e0baece0f863cf5a36285c5b3e200508ed8ef6cc
kynitta <kynitta@yahoo-corp.jp>
2023-02-14 14:38:08 +0900

    add v10

--- report_sample.py
+++ report_sample.py
@@ -4,6 +4,13 @@ import time
 import sys
 import configparser
 import openapi_client
+from openapi_client.api.report_definition_service_api import ReportDefinitionServiceApi
+from openapi_client.model.report_definition import ReportDefinition
+from openapi_client.model.report_definition_service_download_selector import ReportDefinitionServiceDownloadSelector
+from openapi_client.model.report_definition_service_operation import ReportDefinitionServiceOperation
+from openapi_client.model.report_definition_service_report_date_range_type import ReportDefinitionServiceReportDateRangeType
+from openapi_client.model.report_definition_service_report_job_status import ReportDefinitionServiceReportJobStatus
+from openapi_client.model.report_definition_service_selector import ReportDefinitionServiceSelector
 from openapi_client.rest import ApiException
 from pprint import pprint
 config_ini = configparser.ConfigParser()
@@ -18,18 +25,18 @@ configuration.host = "https://ads-display.yahooapis.jp/api/v9"
 # Enter a context with an instance of the API client
 with openapi_client.ApiClient(configuration) as api_client:
     # Create an instance of the API class
-    api_instance = openapi_client.ReportDefinitionServiceApi(api_client)
+    api_instance = ReportDefinitionServiceApi(api_client)
     job_id = 0
 
     # Add
-    report_definition = openapi_client.ReportDefinition(
-        report_date_range_type=openapi_client.ReportDefinitionServiceReportDateRangeType.YESTERDAY,
+    report_definition = ReportDefinition(
+        report_date_range_type=ReportDefinitionServiceReportDateRangeType('YESTERDAY'),
         report_name="sample",
         fields=["IMPS","CLICK"],
     )
 
-    report_definition_service_operation = openapi_client.ReportDefinitionServiceOperation(
-        account_id=config_ini['DEFAULT']['account_id'],
+    report_definition_service_operation = ReportDefinitionServiceOperation(
+        account_id=int(config_ini['DEFAULT']['account_id']),
         operand=[report_definition]
     ) # ReportDefinitionServiceOperation |  (optional)
 
@@ -40,8 +47,8 @@ with openapi_client.ApiClient(configuration) as api_client:
         print("Exception when calling ReportDefinitionServiceApi->report_definition_service_add_post: %s\n" % e)
 
     # Get
-    report_definition_service_selector = openapi_client.ReportDefinitionServiceSelector(
-        account_id=config_ini['DEFAULT']['account_id'],
+    report_definition_service_selector = ReportDefinitionServiceSelector(
+        account_id=int(config_ini['DEFAULT']['account_id']),
         report_job_ids=[job_id]
     ) # ReportDefinitionServiceSelector |  (optional)
 
@@ -49,7 +56,7 @@ with openapi_client.ApiClient(configuration) as api_client:
         api_response = api_instance.report_definition_service_get_post(report_definition_service_selector=report_definition_service_selector)
 
         num = 0
-        while api_response.rval.values[0].report_definition.report_job_status != openapi_client.ReportDefinitionServiceReportJobStatus.COMPLETED:
+        while api_response.rval.values[0].report_definition.report_job_status != ReportDefinitionServiceReportJobStatus('COMPLETED'):
             time.sleep(1)
             num += 1
             if num >= 100:
@@ -60,8 +67,8 @@ with openapi_client.ApiClient(configuration) as api_client:
         print("Exception when calling ReportDefinitionServiceApi->report_definition_service_get_post: %s\n" % e)
 
     # download
-    report_definition_service_download_selector = openapi_client.ReportDefinitionServiceDownloadSelector(
-        account_id=config_ini['DEFAULT']['account_id'],
+    report_definition_service_download_selector = ReportDefinitionServiceDownloadSelector(
+        account_id=int(config_ini['DEFAULT']['account_id']),
         report_job_id=job_id
     ) # ReportDefinitionServiceDownloadSelector |  (optional)
 
